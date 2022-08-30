03e725f2067d44f510fb420e27e84f93a9ffe05e
jiuyun <jiuyunhziee@163.com>
2022-08-30 15:00:24 +0800

    BT同步2时，同步函数中取消BT使能

--- components/chip/drivers/bt.c
+++ components/chip/drivers/bt.c
@@ -399,7 +399,7 @@ csi_error_t csi_bt_set_sync(csp_bt_t *ptBtBase,csi_bt_trgin_e eTrgin, csi_bt_trg
 				(eTrgMode << BT_OSTMD_POS(eTrgin));
 		
 		ptBtBase->CR |= BT_EXTCKM_MSK;		//selecet count clk source
-		ptBtBase->RSSR |= BT_START;			//start bt	
+		//ptBtBase->RSSR |= BT_START;			//start bt	
 	}
 	else
 		return CSI_ERROR;
