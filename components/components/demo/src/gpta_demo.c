df59433070d63116780d10f0e6f046d0a9ede591
yuanym <yuanye.min@163.com>
2023-06-12 13:30:14 +0800

    添加GPTA 捕获低电平的实例，同时优化demo注释

--- components/components/demo/src/gpta_demo.c
+++ components/components/demo/src/gpta_demo.c
@@ -38,15 +38,16 @@ int gpta_timer_demo(void)
 	return iRet;	
 }
 
-/** \brief GPTA捕获示例代码
- *   		- 捕获2次产生一次捕获中断
+/** \brief GPTA捕获示例代码， 测量周期时间
+ *   		- 捕获4次产生一次捕获中断，ldbarst捕获后，计数器进行重置
  *     		- 由PA01触发外部事件1，经过ETCB  触发sync2 捕获
  * 			- 信号由PA01的高低电平切换产生（一直高电平意味着没有触发）
+ *          - CMPA捕获的是第一次周期值，CMPB捕获的是第二次周期值，CMPAA捕获的是第三次周期值,CMPBA捕获的是第四次周期值
  *  \param[in] none
  *  \return error code
  */
 
-int gpta_capture_demo(void)
+int gpta_capture_sync_demo0(void)
 {
 	int iRet = 0;	
     volatile uint8_t ch;
@@ -98,6 +99,84 @@ int gpta_capture_demo(void)
 //	tpFiltercfg.byWinWidth       =0xffff/2;
 //    csi_gpta_set_sync_filter(GPTA0, &tpFiltercfg);
 //------------------------------------------------------------------------------------------------------------------------	
+	csi_gpta_start(GPTA0);//start  timer
+    while(1){		
+		  		      
+		    mdelay(200);                        
+		    	
+		    mdelay(200);
+	}			
+	return iRet;
+};
+
+/** \brief GPTA捕获示例代码，测试低电平时间
+ *   		- 捕获1次产生一次捕获中断，ldarst捕获后，计数器进行重置
+ *     		- 由PA01下降沿产生外部事件1，经过ETCB  触发sync0，重置和启动计数器
+ *          - 由PA01外部扩展口，上升沿产生外部事件5，经过ETCB  触发sync2 捕获，上升沿捕获值存放在CMPA中
+ * 			- 信号由PA01的高低电平切换产生（一直高或低电平意味着没有触发）
+ *          - CMPA捕获的是下降沿时间
+ *  \param[in] none
+ *  \return error code
+ */
+int gpta_capture_sync_demo1(void)
+{
+	int iRet = 0;	
+    volatile uint8_t ch;
+
+
+	csi_pin_set_mux(PA01,PA01_INPUT);		
+	csi_pin_pull_mode(PA01, GPIO_PULLUP);						//PA1 上拉
+	
+	csi_pin_irq_mode(PA01,EXI_GRP1, GPIO_IRQ_FALLING_EDGE);		//PA1 下降沿产生中断 
+	csi_pin_irq_enable(PA01, ENABLE);	
+	csi_exi_set_evtrg(0, TRGSRC_EXI1, 1);		
+	
+	csi_pin_irq_mode(PA01, EXI_GRP16, GPIO_IRQ_RISING_EDGE);     //PA1 上升沿产生中断，选择中断组16
+	csi_pin_irq_enable(PA01, ENABLE);                            //PA1 中断使能                                    
+	csi_exi_set_evtrg(5, TRGSRC_EXI16, 1);	 
+
+	csi_etb_config_t tEtbConfig;
+	
+//------------------------------------------------------------------------------------------------------------------------		
+	//ETB 参数配置结构体	
+	tEtbConfig.byChType  = ETB_ONE_TRG_ONE;  	//单个源触发单个目标
+	tEtbConfig.bySrcIp   = ETB_EXI_TRGOUT0 ;  	//...作为触发源
+	tEtbConfig.byDstIp   =  ETB_GPTA0_SYNCIN0;  //GPTA0 同步输入0作为目标事件
+	tEtbConfig.byTrgMode = ETB_HARDWARE_TRG;
+	csi_etb_init();
+	ch = csi_etb_ch_alloc(tEtbConfig.byChType);	//自动获取空闲通道号,ch >= 0 获取成功						//ch < 0,则获取通道号失败		
+	iRet = csi_etb_ch_config(ch, &tEtbConfig);			
+	
+//------------------------------------------------------------------------------------------------------------------------		
+	tEtbConfig.byChType  = ETB_ONE_TRG_ONE;  	//单个源触发单个目标
+	tEtbConfig.bySrcIp   = ETB_EXI_TRGOUT5 ;  	//...作为触发源
+	tEtbConfig.byDstIp   =  ETB_GPTA0_SYNCIN2;  //GPTB0 同步输入2作为目标事件
+	tEtbConfig.byTrgMode = ETB_HARDWARE_TRG;
+	csi_etb_init();
+	ch = csi_etb_ch_alloc(tEtbConfig.byChType);	//自动获取空闲通道号,ch >= 0 获取成功						//ch < 0,则获取通道号失败		
+	iRet = csi_etb_ch_config(ch, &tEtbConfig);	
+//------------------------------------------------------------------------------------------------------------------------	
+	csi_gpta_captureconfig_t tPwmCfg;								  
+	tPwmCfg.byWorkmod       = GPTA_CAPTURE;                     //WAVE or CAPTURE    //计数或捕获	
+	tPwmCfg.byCountingMode  = GPTA_UPCNT;                       //CNYMD  //计数方向
+	tPwmCfg.byOneshotMode    = GPTA_OP_CONT; 
+	tPwmCfg.byStartSrc      = GPTA_SYNC;				    //软件使能同步触发使能控制（RSSR中START控制位）//启动方式
+	tPwmCfg.byPscld         = GPTA_LDPSCR_ZRO;                  //PSCR(分频)活动寄存器载入控制。活动寄存器在配置条件满足时，从影子寄存器载入更新值	
+	tPwmCfg.byCaptureCapmd   = 0;                               //0:连续捕捉模式    1h：一次性捕捉模式
+	tPwmCfg.byCaptureStopWrap=1-1;                              //Capture模式下，捕获事件计数器周期设置值
+	tPwmCfg.byCaptureLdaret   =1;                                //CMPA捕捉载入后，计数器值计数状态控制位(1h：CMPA触发后，计数器值进行重置;0h：CMPA触发后，计数器值不进行重置)
+	tPwmCfg.byCaptureLdbret   =0; 
+	tPwmCfg.byCaptureLdaaret  =0;  
+	tPwmCfg.byCaptureLdbaret  =0;  
+	tPwmCfg.wInt 		 =GPTA_INTSRC_CAPLD0;                   //interrupt//
+		
+	csi_gpta_capture_init(GPTA0, &tPwmCfg);
+
+	csp_gpta_set_phsr(GPTA0, 0x00);
+	csp_gpta_phsen_enable(GPTA0, ENABLE);
+//------------------------------------------------------------------------------------------------------------------------
+	csi_gpta_set_sync(GPTA0, GPTA_TRG_SYNCEN0, GPTA_TRG_CONTINU, GPTA_AUTO_REARM_ZRO);//使能SYNCIN0外部触发
+    csi_gpta_set_sync(GPTA0, GPTA_TRG_SYNCEN2, GPTA_TRG_CONTINU, GPTA_AUTO_REARM_ZRO);//使能SYNCIN2外部触发
 	csi_gpta_start(GPTA0);//start  timer
     while(1){		
 		  		      
