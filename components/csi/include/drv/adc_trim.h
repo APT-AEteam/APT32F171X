1eb7bf8d9e3081b9ac582734f74b0bc9baf0f6b8
Andreas-hsiao <807064338@qq.com>
2023-08-30 15:45:50 +0800

    添加ADC OFFSET校准静态库及校准函数adc0_offset_trim

---
+++ components/csi/include/drv/adc_trim.h
@@ -0,0 +1,15 @@
+/***********************************************************************//** 
+ * \file  adc_trim.h
+ * \brief  csi adc driver head file
+ * \copyright Copyright (C) 2015-2023 @ APTCHIP
+ * <table>
+ * <tr><th> Date  <th>Version  <th>Author  <th>Description
+ * <tr><td> 2023-8-29 <td>V0.0  <td>XS   <td>initial
+ * </table>
+ * *********************************************************************
+*/
+#ifndef _TRIM_ADC_H_
+#define _TRIM_ADC_H_
+
+extern void adc0_offset_trim();
+#endif
